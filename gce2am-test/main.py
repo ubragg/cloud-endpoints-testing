232cdb9793dcc79394905fb4b73ed5d608101161
ubragg <rich.bragg@gmail.com>
2018-03-22 14:43:41 -0700

    clone gce2am folder from gce2

---
+++ gce2am-test/main.py
@@ -0,0 +1,19 @@
+import time
+import endpoints
+
+from protorpc import remote
+from protorpc.message_types import DateTimeMessage
+from protorpc.message_types import VoidMessage
+
+
+@endpoints.api(name="gce2", version="v1", description="GCE 2.0 Test API",
+               allowed_client_ids=[endpoints.API_EXPLORER_CLIENT_ID])
+class Endpoints2TestApi(remote.Service):
+
+  @endpoints.method(VoidMessage, DateTimeMessage, path='get_time', http_method='GET')
+  def get_server_time(self, unused):
+    return DateTimeMessage(milliseconds=int(time.time()*1000.0))
+
+
+rpc_app = endpoints.api_server([Endpoints2TestApi])
+
