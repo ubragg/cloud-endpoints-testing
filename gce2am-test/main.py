4acce88005d5945e9cf032a54964fef785c1499c
ubragg <rich.bragg@gmail.com>
2018-03-22 14:44:49 -0700

    add API Management to gce2am

--- gce2am-test/main.py
+++ gce2am-test/main.py
@@ -6,14 +6,14 @@ from protorpc.message_types import DateTimeMessage
 from protorpc.message_types import VoidMessage
 
 
-@endpoints.api(name="gce2", version="v1", description="GCE 2.0 Test API",
+@endpoints.api(name="gce2am", version="v1", description="GCE 2.0 AM Test API",
                allowed_client_ids=[endpoints.API_EXPLORER_CLIENT_ID])
-class Endpoints2TestApi(remote.Service):
+class Endpoints2AMTestApi(remote.Service):
 
   @endpoints.method(VoidMessage, DateTimeMessage, path='get_time', http_method='GET')
   def get_server_time(self, unused):
     return DateTimeMessage(milliseconds=int(time.time()*1000.0))
 
 
-rpc_app = endpoints.api_server([Endpoints2TestApi])
+rpc_app = endpoints.api_server([Endpoints2AMTestApi])
 
